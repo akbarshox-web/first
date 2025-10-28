e7c7ddf133aa8501b04e06eedc33cd161326216b
hakimov0545 <abdulazizhakimov13@gmail.com>
2025-10-28 18:07:49 +0500

    Add button qoshildi

--- src/pages/teachers.page.jsx
+++ src/pages/teachers.page.jsx
@@ -1,40 +1,46 @@
-import { Table } from "antd";
+import { Table, Button } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const columns = [{
-    title: 'Id',
-    dataIndex: 'id',
-    key: 'id',
-}, {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-}, {
-    title: 'Surname',
-    dataIndex: 'surname',
-    key: 'surname',
-}]
+const columns = [
+	{
+		title: "Id",
+		dataIndex: "id",
+		key: "id",
+	},
+	{
+		title: "Name",
+		dataIndex: "name",
+		key: "name",
+	},
+	{
+		title: "Surname",
+		dataIndex: "surname",
+		key: "surname",
+	},
+];
 
 function TeachersPage() {
-    const [teachers, setTeachers] = useState([]);
+	const [teachers, setTeachers] = useState([]);
 
+	async function getTeachers() {
+		const rec = await axios.get(
+			"https://037771c025a6e640.mokky.dev/teachers"
+		);
+		setTeachers(rec.data);
+	}
+	useEffect(() => {
+		getTeachers();
+	}, []);
 
-    async function getTeachers() {
-        const rec = await axios.get("https://037771c025a6e640.mokky.dev/teachers");
-        setTeachers(rec.data);
-    }
-    useEffect(() => {
-        getTeachers();
-    }, [])
-
-    return (
-        <div>
-            <div>
-                <h2>teachers</h2>
-            </div>
-            <Table dataSource={teachers} columns={columns} />
-        </div>
-    )
+	return (
+		<div>
+			<div>
+				<h2>teachers</h2>
+				<Button>Add</Button>
+			</div>
+			<Table dataSource={teachers} columns={columns} />
+		</div>
+	);
 }
 export default TeachersPage;
